07e33c6955325af3cb58c8dec3fc8f1a6805272d
gllisboa <guilhermeluzlisboa@gmail.com>
2023-09-13 00:01:02 +0100

    modified:   login.js

---
+++ pesquisa.js
@@ -0,0 +1,10 @@
+export default class PagePesquisa {
+
+    abrir() {
+        console.log("Abrir url de login: http://publicazo.insprak.com/ ");
+    }
+
+    realizarPesquisa(termo) {
+        console.log("Realiza a pesquisa do " + termo);
+    }
+}
